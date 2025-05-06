68ef8fdb3df3922553077815dc452c85f3526181
Siddharth Vijay Mane <150709182+siddharthmane17@users.noreply.github.com>
2025-05-06 23:42:19 +0200

    Update run_all.py. needs to be checked in PPO

--- run_all.py
+++ run_all.py
@@ -7,6 +7,6 @@ os.system("python3 -m drl.agents.pp_agent")
 print("\nStep 2: Testing Feature Extractor")
 os.system("python3 test_feature_extractor.py")
 
-print("\n Step 3: Launching TensorBoard (optional)")
+print("\n Step 3: Launching TensorBoard)
 print("To run TensorBoard manually, use:")
 print("tensorboard --logdir=wasmweaver_tensorboard")
