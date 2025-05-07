4cb5cf5a549c37e0db1d03903ec22f96091ea3f5
Siddharth Vijay Mane <150709182+siddharthmane17@users.noreply.github.com>
2025-05-07 09:49:37 +0200

    Updated  run_all.py (test pipeline)

--- run_all.py
+++ run_all.py
@@ -1,12 +1,12 @@
 import os
 
 print(" Step 1: Training PPO Agent")
-os.system("python3 -m drl.agents.pp_agent")
+os.system("python3 -m drl.agents.ppo_agent")
 
 
 print("\nStep 2: Testing Feature Extractor")
 os.system("python3 test_feature_extractor.py")
 
-print("\n Step 3: Launching TensorBoard)
+print("\n Step 3: Launching TensorBoard (optional)")
 print("To run TensorBoard manually, use:")
 print("tensorboard --logdir=wasmweaver_tensorboard")
